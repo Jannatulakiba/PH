26b585470959edf0b4d1e95a741e4682c7bad18f
jannatul akiba <jannatulakiba70023@gmail.com>
2026-07-17 18:28:32 +0600

    modules_8

---
+++ practice_task_module_8/variable.js
@@ -0,0 +1,218 @@
+let price1 = 100;
+let price2 = 200;
+
+// if (price1 == price2) {
+//     console.log("valid price");
+// }
+// else {
+//     console.log("invalid");
+// }
+
+// if (price1 === price2) {
+//     console.log("valid price");
+// }
+// else {
+//     console.log("invalid");
+// }
+
+// if (price1 != price2) {
+//     console.log("valid price");
+// }
+// else {
+//     console.log("invalid");
+// }
+
+// if (price1 > price2) {
+//     console.log("valid price");
+// }
+// else {
+//     console.log("invalid");
+// }
+// if (price1 <= price2) {
+//     console.log("valid price");
+// }
+// else {
+//     console.log("invalid");
+// }
+
+// console.log("5" == 5);   // true
+// console.log("5" === 5);  // false
+
+// const isRaining = true
+
+// if (isRaining) {
+//     console.log("Take an umbrella")
+// }
+// else {
+//     console.log("today no rain")
+// }
+
+// let stock = 0;
+// if (stock === 0) {
+//     console.log("Out of stock")
+// } 
+
+// let plot = 2.00;
+// if (plot > 0) {
+//     console.log("value is :", plot)
+// }
+// else {
+//     console.log("value is :", plot)
+// }
+// let year = 2026;
+// if (year % 4 == 0) {
+//     console.log("it is leap year")
+// }
+// else {
+//     console.log("not leap year")
+// }
+// let year1 = 2026;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     console.log("It is a leap year");
+// } else {
+//     console.log("Not a leap year");
+// }
+// let speed = 80;
+// if (speed > 80) {
+//     console.log("overspeeding")
+// }
+// else {
+//     console.log("Normal speed")
+// }
+
+
+// let age = 20
+// let hasTicket = true
+
+// if (age > 18 && hasTicket) {
+//     console.log("the persone has ticket")
+
+// }
+// else {
+//     ("persone dont have ticket")
+// }
+
+// let isWeekend = true;
+// let isHoliday = true;
+
+// if (isWeekend || isHoliday) {
+//     console.log("No work today");
+// } else {
+//     console.log("Go to work");
+// }
+
+// let username = true
+// let password = true
+
+
+// if (username && password) {
+//     console.log("successfull")
+// }
+
+// let user = {
+//     username: "admin",
+//     password: "1234"
+// };
+
+// let inputUsername = "admin";
+// let inputPassword = "1234";
+
+// if (
+//     user.username === inputUsername &&
+//     user.password === inputPassword
+// ) {
+//     console.log("Login successful");
+// } else {
+//     console.log("Login failed");
+// }
+
+
+let marks = 75;
+let grade;
+if (marks >= 80) {
+    grade = "A+";
+}
+else if (marks >= 70) {
+    grade = "A";
+}
+else if (marks >= 60) {
+    grade = "B";
+}
+else if (marks >= 50) {
+    grade = "C";
+}
+else {
+    grade = "F";
+}
+
+// console.log("output grade : ", grade);
+
+// let month = 7;
+
+// if (month === 12 || month === 1) {
+//     console.log("Winter");
+// }
+// else if (month === 2 || month === 3) {
+//     console.log("Spring");
+// }
+// else if (month === 4 || month === 5) {
+//     console.log("Summer");
+// }
+// else if (month === 6 || month === 7) {
+//     console.log("Monsoon");
+// }
+// else if (month === 8 || month === 9) {
+//     console.log("Autumn");
+// }
+// else if (month === 10 || month === 11) {
+//     console.log("Late Autumn");
+// }
+// else {
+//     console.log("Invalid month");
+// }
+
+// let age = 18;
+// let hasID = true;
+
+// if (age >= 18) {
+//     if (hasID) {
+//         console.log("Eligible to vote");
+//     } else {
+//         console.log("Not eligible: No ID");
+//     }
+// }
+// else {
+//     console.log("Not eligible: Under 18");
+// }
+
+// let isLoggedIn = true
+// let isAdmin = true
+
+// if (isLoggedIn) {
+//     if (isAdmin) {
+//         console.log("Admin Dashboard")
+//     }
+//     else {
+//         console.log("user Dashboard")
+//     }
+// }
+// else {
+//     console.log("logging failed")
+// }
+
+// let number = 5;
+
+// let result = (number % 2 === 0) ? "Even" : "Odd";
+
+// console.log("Print number is:", result);
+
+// let age = 20;
+
+// let validAge = (age >= 18) ? "adult" : "minor";
+
+// console.log("people status : ", validAge)
+
+let age = 2000;
+
+console.log(age >= 1000 ? "Expensive" : "affotable");
