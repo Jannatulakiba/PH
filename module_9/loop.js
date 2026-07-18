269eb6ab1fcbbece6ae4e9c5c847502ad5aa6fb4
jannatul akiba <jannatulakiba70023@gmail.com>
2026-07-18 23:49:26 +0600

    loop

---
+++ module_9/loop.js
@@ -0,0 +1,48 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log(i + " Hello World");
+// }
+
+// let i = 899;
+// while (i < 903) {
+//      console.log("i value is", i);
+//     i++;
+// }
+
+// for (let i = 2; i < 10; i = i + 2) {
+//     // console.log('all value is ', i);
+// }
+
+// let sum = 0;
+// for (let i = 2; i < 10; i = i + 2) {
+//     sum = sum + i;
+//     // console.log('i value is ', i, ' and sum value is ', sum);
+// }
+
+// let num = 3;
+// for (i = 0; i <= 10; i++) {
+//     // console.log(num , 'X' , i , '=' , i * num);
+
+// }
+
+
+// for (let i = 0; i < 100; i++) {
+//     if (i > 5) {
+//         break;
+//     }
+
+//     // console.log("output value : " , i);
+// }
+
+// // for(let i = 0; i < 10; i++){
+// //     if(i === 5){
+// //         continue;
+// //     }
+
+// //     // console.log("output value : " , i);
+
+let i = 0;
+
+do {
+    console.log("value : ", i);
+    i++;
+} while (i > 5);
