48aaa3ff21fde3228a22cc13338fb095b7356ed8
jannatul akiba <jannatulakiba70023@gmail.com>
2026-07-27 20:09:33 +0600

    array

---
+++ practice_task_module_10/begginer_array.js
@@ -0,0 +1,55 @@
+// const number = [1, 2, 3, 4, 5];
+// console.log(number);
+// const names = ['akiba', 'abir', 'safa']
+// console.log(name);
+// const id = [21, 4, 78];
+// console.log(id);
+
+// 2. Log the length of the array from Task 1.
+
+// console.log(names.length);
+
+// 3. Log the item at index 0 and index 2 of the array.
+// console.log(names[0], names[2]);
+
+// 4. Change the item at index 1 to a new value. Log the array.
+//  names[1] = 'basudha';
+// console.log(names[1]);
+
+// 5. Add one item to the end of the array. Log the array.
+const fruits = ["apple","mangoo", "bananna"]
+// fruits.push("komla")
+// console.log(fruits);
+
+//6.remove last one
+
+// fruits.pop()
+// console.log(fruits)
+
+// 7. Add one item to the start of the array. Log the array.
+// fruits.unshift("komla");
+// console.log(fruits);
+
+// 8. Remove the first item from the array. Log the array and the removed value.
+// fruits.shift()
+// console.log(fruits);
+
+// 9. Check whether the array contains a specific value. Log true or false.
+
+// console.log(fruits.includes("apple"))
+
+// 10.Find the position of a specific value in the array. Log the number.
+// console.log(fruits.indexOf("banana"));
+
+// 11. Loop through the array with a for loop and log each item.
+// for(let i = 0; i < fruits.length; i ++){
+//     console.log(fruits[i]);
+// }
+
+// 12. while loop
+// let i =0;
+// while( i < fruits.length){
+//     console.log(fruits[i])
+//     i++;
+// }
+
---
+++ practice_task_module_10/intermediate.js
@@ -0,0 +1,16 @@
+// 1. Combine two arrays into one new array. Log the result.
+const numb = [22, 55, 77, 99]
+const names = ["akiba" , "safa" , "abir"]
+// const result = numb.concat(nam)
+// console.log(result)
+
+// 2. Take a portion of an array (not the whole thing) without changing the original. Log the result.
+// const number = numb.slice(1,2)
+// console.log(number)
+
+// 3. Turn an array of words into a single sentence string.
+// Expected: a string like "word word word"
+
+// const words = ["javascripts" , "is" , "fun"]
+// const sentence = words.join("   ")
+// console.log(sentence)
